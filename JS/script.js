b5154c909a79188fc0658833b85638532e32b514
DESKTOP-65SAUST\Alumno <victor2003aguilar@gmail.com>
2025-01-30 15:21:32 +0100

    Prueba traductor subiendo y click
    El traductor sube en el juego, se espera, cuando hay un click baja

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,18 @@
 $(document).ready(function(){
     console.log("jQuery Funciona");
     
-
-
-
-
-
-
-
 })
+
+document.addEventListener("DOMContentLoaded", () => {
+    const image = document.getElementById("toggleImage");
+    setTimeout(() => {
+        image.classList.remove("hidden");
+        image.classList.add("visible");
+        setTimeout(() => {
+            image.addEventListener("click", () => {
+                image.classList.remove("visible");
+                image.classList.add("hidden");
+            });
+        }, 7000);
+    }, 3000);
+});
