141d350ffd7c8e6be37b96bb97c89974770edf67
yoyi3d <ascargortageorgy@escivi.com>
2025-02-10 22:49:47 +0100

    Casi terminado
    Falta por terminar
    - faq con integración js
    - boton de jugar demo de forma funcional
    - reajustar formulario
    - terminar footer

--- JS/script.js
+++ JS/script.js
@@ -3,16 +3,16 @@ $(document).ready(function(){
     
 })
 
-document.addEventListener("DOMContentLoaded", () => {
-    const image = document.getElementById("toggleImage");
-    setTimeout(() => {
-        image.classList.remove("hidden");
-        image.classList.add("visible");
-        setTimeout(() => {
-            image.addEventListener("click", () => {
-                image.classList.remove("visible");
-                image.classList.add("hidden");
-            });
-        }, 7000);
-    }, 3000);
-});
+// document.addEventListener("DOMContentLoaded", () => {
+//     const image = document.getElementById("toggleImage");
+//     setTimeout(() => {
+//         image.classList.remove("hidden");
+//         image.classList.add("visible");
+//         setTimeout(() => {
+//             image.addEventListener("click", () => {
+//                 image.classList.remove("visible");
+//                 image.classList.add("hidden");
+//             });
+//         }, 7000);
+//     }, 3000);
+// });
