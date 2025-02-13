0884170d7d2c3d2f5d4c3eacbfd6b3d8b76177ab
DavidBarriosPecho <fox123susto@gmail.com>
2025-02-13 19:51:45 +0100

    Añadido Sonido y Efectos hover en el menú
    Tambien cada personaje dice una voz si le das click

--- JS/script.js
+++ JS/script.js
@@ -17,9 +17,18 @@ $(document).ready(function(){
 //     }, 3000);
 // });
 
+const clickSound = new Audio('./Media/Sonidos/ClickBoton.mp3');
+
+function playClickSound() {
+    clickSound.currentTime = 0; 
+    clickSound.play(); 
+}
+
+
 /* DATOS DE LOS PERSONAJES */
 
-let selectedCharacter = 0; 
+
+selectedCharacter = 0;
 const characterNames = [
     "Huesito",
     "Lepion",
@@ -35,29 +44,7 @@ function updateCharacter(characterIndex) {
     updatePlayButton();
 }
 
-function updatePlayButton() {
-    const playButton = $("#button-play");
 
-    if (selectedCharacter === 0) {
-        playButton.css({
-            "background-color": "black",
-            "cursor": "not-allowed",
-            "opacity": "0.5"
-        }).off("click"); 
-    } else {
-        playButton.css({
-            "background-color": "#2c3434",
-            "cursor": "pointer",
-            "opacity": "1",
-            "font-family": "'Chewy', serif",  
-            "color": " #e8f8d8"
-        }).text("ADELANTE GRANUJA")  
-        .off("click")
-        .on("click", function() {
-            window.location.href = "juego.html"; 
-        });
-    }
-}
 
 /* CAMBIAR PAGINA A CARTAS */
 document.addEventListener("DOMContentLoaded", function () {
@@ -65,7 +52,10 @@ document.addEventListener("DOMContentLoaded", function () {
     
     if (button) {
         button.addEventListener("click", function () {
-            window.location.href = "cartas.html";
+            playClickSound();
+            setTimeout(() => {
+                window.location.href = "cartas.html";
+            }, 300);     
         });
     } else {
         console.error("El botón con ID 'select-payment' no se encontró.");
@@ -73,10 +63,12 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
-
+/* CAMBIAR PAGINA A PERSONAJES */
 $("#select-client").on("click", function() {
-    selectedCharacter = (selectedCharacter % 5) + 1; 
-    updateCharacter(selectedCharacter);
+   playClickSound();
+    setTimeout(() => {
+        window.location.href = "characters.html"; 
+    }, 300); 
 });
 
 updateCharacter(selectedCharacter);
@@ -84,7 +76,6 @@ updateCharacter(selectedCharacter);
 
 
 /* FLIPEAR CARTAS POR CLICK */
-
 const shopCards = document.querySelectorAll('.shopcard');
 let gameOver = false; 
 
@@ -107,7 +98,85 @@ shopCards.forEach(card => {
 
             setTimeout(() => {
                 window.location.href = 'menu.html';
-            }, 3000);
+            }, 1500);
+        }
+    });
+});
+
+//EFECTOS BOTONES MENÚ
+
+const selectClientButton = document.getElementById('select-client');
+const selectPaymentButton = document.getElementById('select-payment');
+const buttonPlay = document.getElementById('button-play');
+
+
+function addHoverEffect(button) {
+    button.addEventListener('mouseover', () => {
+        button.style.backgroundColor = 'grey'; 
+        button.style.cursor = 'pointer'; 
+    });
+
+    button.addEventListener('mouseout', () => {
+        button.style.backgroundColor = ''; 
+    });
+}
+
+addHoverEffect(selectClientButton);
+addHoverEffect(selectPaymentButton);
+
+function updateButtonPlayState() {
+    if (selectedCharacter !== 0) {
+        addHoverEffect(buttonPlay);
+    } else {
+        buttonPlay.removeEventListener('mouseover', () => {});
+        buttonPlay.removeEventListener('mouseout', () => {});
+    }
+}
+
+
+function updatePlayButton() {
+    const playButton = $("#button-play");
+
+    if (selectedCharacter === 0) {
+        playButton.css({
+            "cursor": "not-allowed",
+            "opacity": "0.5"
+        }).off("click"); 
+    } else {
+        playButton.css({
+            "cursor": "pointer",
+            "opacity": "1",
+            "font-family": "'Chewy', serif",  
+            "color": " #e8f8d8"
+        }).text("ADELANTE GRANUJA")  
+        .off("click")
+        .on("click", function() {
+            playClickSound();
+            setTimeout(() => {
+                window.location.href = "juego.html"; 
+            }, 300);       
+        });
+        updateButtonPlayState();
+    }
+}
+
+
+// VOCES PERSONAJE
+
+const soundsCharacter = {
+    'charactermenu-1': new Audio('./Media/Sonidos/Voz1.mp3'),
+    'charactermenu-2': new Audio('./Media/Sonidos/Voz2.mp3'),
+    'charactermenu-3': new Audio('./Media/Sonidos/Voz3.mp3'),
+    'charactermenu-4': new Audio('./Media/Sonidos/Voz4.mp3'),
+    'charactermenu-5': new Audio('./Media/Sonidos/Voz5.mp3')
+};
+
+document.querySelectorAll('.characterMenu').forEach((character) => {
+    character.addEventListener('click', () => {
+        const soundCharacter = soundsCharacter[character.id]; 
+        if (soundCharacter) {
+            soundCharacter.currentTime = 0; 
+            soundCharacter.play(); 
         }
     });
 });
