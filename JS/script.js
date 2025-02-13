e0d74d74087072480b2f25e64779838fe13444e6
DavidBarriosPecho <fox123susto@gmail.com>
2025-02-13 12:22:10 +0100

    Boton Funcional Menú + HTML Cartas add

--- JS/script.js
+++ JS/script.js
@@ -30,6 +30,31 @@ function updateCharacter(characterIndex) {
     $(".characterMenu").hide();
     $("#charactermenu-" + characterIndex).show();
     $("#text-box").text(characterNames[characterIndex - 1]);
+    updatePlayButton();
+}
+
+function updatePlayButton() {
+    const playButton = $("#button-play");
+
+    if (selectedCharacter === 0) {
+        playButton.css({
+            "background-color": "black",
+            "cursor": "not-allowed",
+            "opacity": "0.5"
+        }).off("click"); 
+    } else {
+        playButton.css({
+            "background-color": "#2c3434",
+            "cursor": "pointer",
+            "opacity": "1",
+            "font-family": "'Chewy', serif",  
+            "color": " #e8f8d8"
+        }).text("ADELANTE GRANUJA")  
+        .off("click")
+        .on("click", function() {
+            window.location.href = "juego.html"; 
+        });
+    }
 }
 
 $("#select-client").on("click", function() {
