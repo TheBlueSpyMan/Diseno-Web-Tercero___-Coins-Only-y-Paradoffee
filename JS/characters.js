01ac226e7f72f36a26dda9af0bdda4afa6d998bd
MirtharCode <80948038+MirtharCode@users.noreply.github.com>
2025-02-14 11:58:22 +0100

    Characters metido

--- JS/characters.js
+++ JS/characters.js
@@ -1,20 +1,40 @@
+const images = ["huesitos", "leemur", "maricarmen", "piojosa", "rabanejo"];
+const chcharacterNames = {
+    "huesitos": "Huesito",    "leemur": "Leemur",
+    "maricarmen": "Maricarmen",    "piojosa": "Piojosa",
+    "rabanejo": "Rabanejo"
+};
+const leverImages = ["lever", "reverse-lever"];
 let currentIndex = 0;
-const characters = document.querySelectorAll(".characterCH");
+let isFlipped = false;
+const charImage = document.querySelector(".ch");
 
-console.log("Personajes detectados:", characters.length);
+function changeCharacter() {    
+    const leverImage = document.querySelector(".lever"); // Asegúrate de que la imagen tenga la clase "lever"
+    if (charImage) {
+        // Mover a la izquierda y hacer fade out
+        
+        charImage.style.transform = "translateX(-500px) scale(2)";
+        charImage.style.opacity = "0";
+        leverImage.src = `Media/Resources/Img/Characters/${leverImages[1]}.png`;
+        // Esperar a que termine la animación antes de cambiar la imagen
+        setTimeout(() => {
+            currentIndex = (currentIndex + 1) % images.length;
+            charImage.src = `Media/Resources/Img/Characters/${images[currentIndex]}.png`;
 
-characters[currentIndex].classList.add("characterCH-active");
+            // Mover hacia la derecha antes de volver a la posición inicial
+            charImage.style.transform = "translateX(500px) scale(2)";
+            
+            setTimeout(() => {
+                charImage.style.transform = "translateX(0) scale(2)"; // Volver a la posición original
+                charImage.style.opacity = "1"; // Volver a hacer visible
+                leverImage.src = `Media/Resources/Img/Characters/${leverImages[0]}.png`;
+            }, 50); // Pequeño retraso para hacer el cambio más natural
+        }, 500); // Esperar 500ms para cambiar la imagen
+    }
+}
 
-function changeCharacter() {
-    console.log("Palanca activada. Cambiando personaje...");
-    
-    characters[currentIndex].classList.remove("characterCH-active");
-    characters[currentIndex].classList.add("characterCH");
-    
-    currentIndex = (currentIndex + 1) % characters.length;
-    
-    characters[currentIndex].classList.add("characterCH-active");
-    characters[currentIndex].classList.remove("characterCH");
-
-    console.log("Nuevo personaje activo:", currentIndex);
+function saveCharacter() {
+    localStorage.setItem("selectedCharacter", currentIndex);
+    window.location.href = "menu.html"; 
 }
